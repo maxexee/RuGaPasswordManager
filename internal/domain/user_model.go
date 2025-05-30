2bd8364fbf99ccaa58c1a70cefab82955c7b6513
maxexee <ruizmmaxm@gmail.com>
2025-05-29 19:13:05 -0600

    Actualización del modelo de Usuarios.

--- internal/domain/user_model.go
+++ internal/domain/user_model.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // STRUCT - MODELO - DE LOS USUARIOS - ES LA REPRESENTACION DE LA TABLA EN LA BD.
 type User struct {
 	gorm.Model
-	Email     string `gorm:"unique"`
-	Passsword string
+	Email     string `gorm:"not null;unique"`
+	Passsword string `gorm:"not null"`
 	Sections  []Section
 }
