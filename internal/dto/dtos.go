03b20786cd8c660a85ef1a41bc7fb8b50e7b9f50
maxexee <ruizmmaxm@gmail.com>
2025-06-05 22:07:23 -0600

    Creation of the login DTO (yes, I know it's the same as the signup dto, but in a future they can be different.

--- internal/dto/dtos.go
+++ internal/dto/dtos.go
@@ -4,3 +4,8 @@ type SignUpDTO struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=12,passwordsFormat"`
 }
+
+type LogInDTO struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
