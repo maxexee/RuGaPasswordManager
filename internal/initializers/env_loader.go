140e9b20a6c0d2b151b3cb36f84f65d0922a5331
maxexee <ruizmmaxm@gmail.com>
2025-07-25 23:19:30 -0600

    In order to load the enviroment variables, have added two ways, for local use, unncomment the local section (It works with gototenv and load the .env directly). If docker is used, use the docker section (it works with O.S. enviroment variables).

--- internal/initializers/env_loader.go
+++ internal/initializers/env_loader.go
@@ -4,19 +4,30 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func EnvLoader() {
-	curDir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// === LOCAL ===
+	// curDir, err := os.Getwd()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	loaderr := godotenv.Load(curDir + "/.env")
-	if loaderr != nil {
+	// loaderr := godotenv.Load(curDir + "/.env")
+	// if loaderr != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// === LOCAL ===
+	// =============
+
+	// === CON DOCKER ===
+	DB_CONNECTION_STRING := os.Getenv("DB_CONNECTION_STRING")
+	SECRET := os.Getenv("SECRET")
+	if DB_CONNECTION_STRING == "" || SECRET == "" {
 		log.Fatal("Error loading .env file")
 	}
+	// === DEV ===
 	fmt.Println("=== TODO BIEN CON .env ===")
+	// === CON DOCKER ===
 }
