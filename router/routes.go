14835ed51a987986a29d9a8eb86faabd01de430f
maxexee <ruizmmaxm@gmail.com>
2025-08-01 14:19:18 -0600

    CORS sections got comment becasue I have put nginx as a reverse proxy for now.

--- router/routes.go
+++ router/routes.go
@@ -9,6 +9,21 @@ import (
 func InitRoutes() *gin.Engine {
 	router := gin.Default()
 
+	// config := cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:3000"},
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           12 * time.Hour,
+	// }
+	// router.Use(cors.New(config))
+
+	// // 2) Manually atender OPTIONS para cualquier ruta (preflight)
+	// router.OPTIONS("/*path", func(c *gin.Context) {
+	// 	c.AbortWithStatus(http.StatusNoContent)
+	// })
+
 	// RUTAS PARA CREACION Y ACCESO DE USUARIO.
 	router.POST("/signup", handler.SignUp)
 	router.POST("/login", handler.Login)
